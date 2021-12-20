32c096dc37dbf73618cc7bb5b10139703d61019f
Johannalindoff <lindoffjohanna@gmail.com>
2021-12-20 20:41:38 +0100

    First

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LunchGuide.Models;
+using User.Models; // Är detta visual studios egna?
+using Microsoft.AspNetCore.Http;
+
+namespace LunchGuide.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
