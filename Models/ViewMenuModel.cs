c847b0964dcab3019599a16d09edc052d5fcf011
emmarosenlind <emma.rosenlind@gmail.com>
2022-01-10 14:56:25 +0100

    Skapa ViewMenuModel

---
+++ Models/ViewMenuModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LunchGuide.Models
+{
+    public class ViewMenuModel
+    {
+        public ViewMenuModel(){}
+
+        public List<DishModel> DishList { get; set; }
+        public RestaurantModel Restaurant { get; set; }
+    }
+}
