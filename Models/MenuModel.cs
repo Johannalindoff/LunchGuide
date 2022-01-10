135e0b7b8fe959aba15181a7a41c2480dfdf79f6
emmarosenlind <emma.rosenlind@gmail.com>
2022-01-10 20:27:50 +0100

    Skapa MenuModel

---
+++ Models/MenuModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunchGuide.Models
+{
+    public class MenuModel
+    {
+        public int Id { get; set; }
+        public String Restaurant { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public String Dish { get; set; }
+        public String DailyMenu { get; set; }
+
+        public List<String> SpecialDiet { get; set; }
+
+    }
+}
